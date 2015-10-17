a966012b2db349341a452ceca35a011189909c25
aKwasny <amkwasiborski@gmail.com>
2015-10-17 19:39:31 +0200

    Nie wiem czy to jest dobrze...

---
+++ Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Andrzej on 2015-10-17.
+ */
+public class Main {
+    public static void main(String[] args) throws WyjatekPustegoStosu {
+        ONP licz = new ONP();
+        licz.Oblicz();
+    }
+}
--- ONP.java
+++ ONP.java
@@ -8,38 +8,37 @@ public class ONP {
     public void Oblicz () throws WyjatekPustegoStosu{
 
         Stack stack = new Stack();
-        double suma = 0;
-        char bierz¹cyZnak;
+        double finalnaLiczba = 0;
+        String bierzacyZnak;
         String napis = "";
         Pair a, b;
 
         System.out.println("Proszê podaj ci¹g.");
         Scanner input = new Scanner(System.in);
         napis = input.nextLine();
-        napis = napis.replaceAll(" ", "");
 
         for (int i = 0; i < napis.length(); i++) {
-            if (napis.charAt(i) == '+' || napis.charAt(i) == '-' || napis.charAt(i) == '*') {
+            if ((napis.equals('+')) || (napis.equals('-')) || (napis.equals('*')) || (napis.equals(':'))) {
                 if (stack.isEmpty()) {
                     throw new WyjatekPustegoStosu("Stos jest pusty. Z³a sk³adania, za ma³o liczb, za du¿o operacji.");
                 } else {
                     a = stack.get();
                     b = stack.get();
-                    if (napis.charAt(i) == '+') {
-                        suma = a.getV() + b.getV();
-                    } else if (napis.charAt(i) == '-') {
-                        suma = a.getV() - b.getV();
+                    if (napis.equals('+')) {
+                        finalnaLiczba = a.getV() + b.getV();
+                    } else if (napis.equals('-')) {
+                        finalnaLiczba = a.getV() - b.getV();
+                    } else if (napis.equals('*')){
+                        finalnaLiczba = a.getV() * b.getV();
                     } else {
-                        suma = a.getV() * b.getV();
+                        finalnaLiczba = a.getV()/b.getV();
                     }
-                    stack.put(suma);
-                    suma = 0;
                 }
             } else {
                 double liczba = Double.valueOf(napis.charAt(i));
                 stack.put(liczba);
             }
         }
-
+        System.out.println("Wynik dzia³ania wynosi " + finalnaLiczba + ".");
     }
 }
